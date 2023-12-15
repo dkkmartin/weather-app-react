175a82b7bde9da931e635117606366eb406bbbd3
Martin <martinbruun1995@gmail.com>
2023-12-15 23:36:59 +0100

    Add .prettierrc, Drawer component, and Pill component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,39 @@
-'use client';
+'use client'
 
-import Weather from '@/components/frontpage-components/Weather';
-import Navbar from '@/components/frontpage-components/Navbar';
-import Image from 'next/image';
+import React, { useCallback } from 'react'
+import Weather from '@/components/frontpage-components/Weather'
+import Navbar from '@/components/frontpage-components/Navbar'
+import Image from 'next/image'
+import { FullScreen, useFullScreenHandle } from 'react-full-screen'
+import Pill from '@/components/frontpage-components/Pill'
 
-export default function Home() {
+export default function App() {
+  const handle = useFullScreenHandle()
+
   return (
-    <main className="h-[100dvh] bg-homepage-background bg-cover flex flex-col items-center">
-      <Weather />
-      <Image
-        className="absolute top-[28%]"
-        src="/assets/house.png"
-        alt="house image"
-        width={390}
-        height={390}
-      ></Image>
-      <Navbar />
-    </main>
-  );
+    <div>
+      <FullScreen handle={handle}>
+        <main className="h-[100dvh] bg-homepage-background bg-cover flex flex-col items-center">
+          <button
+            className="absolute right-4 top-4 bg-transparent border px-2 py-1 rounded-lg font-family-default-regular-body text-white"
+            onClick={handle.enter}
+          >
+            Fullscreen
+          </button>
+          <Weather />
+          <Image
+            className="absolute top-[28%]"
+            src="/assets/house.png"
+            alt="house image"
+            width={390}
+            height={390}
+          ></Image>
+          <Pill
+            weather={{ time: '12 am', percentages: '30', temperature: '19' }}
+          ></Pill>
+          <Navbar />
+        </main>
+      </FullScreen>
+    </div>
+  )
 }
---
+++ src/components/frontpage-components/Pill.tsx
@@ -0,0 +1,31 @@
+import Image from 'next/image'
+
+type Weather = {
+  time: string
+  percentages: string
+  temperature: string
+}
+
+export default function Pill({ weather }: { weather: Weather }) {
+  const { time, percentages, temperature } = weather
+
+  return (
+    <div className="z-10 flex flex-col items-center justify-center border border-weather-colorsolid1 rounded-full bg-weather-colorsolid2 h-[150px] w-[60px] text-white shadow-lg shadow-black py-4 px-2 gap-4">
+      <h1 className="default-bold-subheadline whitespace-nowrap">
+        {time.toUpperCase()}
+      </h1>
+      <div className="flex flex-col items-center">
+        <Image
+          src="/assets/pill/Moon-cloud-fast-wind.svg"
+          width={32}
+          height={32}
+          alt="Night, cloudy, fast wind"
+        ></Image>
+        <h2 className="default-bold-subheadline text-sky-300">
+          {percentages}%
+        </h2>
+      </div>
+      <h3 className="default-bold-subheadline">{temperature}°</h3>
+    </div>
+  )
+}
